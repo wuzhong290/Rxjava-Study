16c758688969b4d93ce1040062ad17a23a1375f9
wuzhong <419479707@qq.com>
2017-06-16 18:22:17 +0800

    demo

--- src/main/java/com/rxjava/examples/ObservableConcat.java
+++ src/main/java/com/rxjava/examples/ObservableConcat.java
@@ -10,13 +10,14 @@ import org.apache.commons.lang.StringUtils;
  * Created by wuzhong on 2017/6/16.
  */
 public class ObservableConcat {
-    static String memoryCache = "1";
+    static String memoryCache = "";
     public static void main(String[] args) {
         Observable<String> memory = Observable.create(new ObservableOnSubscribe<String>(){
             @Override
             public void subscribe(@NonNull ObservableEmitter<String> e) throws Exception {
                 if(StringUtils.isNotBlank(memoryCache)){
                     e.onNext(memoryCache);
+                    e.onComplete();
                 }else{
                     e.onComplete();
                 }
@@ -26,6 +27,7 @@ public class ObservableConcat {
         Observable<String> network = Observable.just("network");
 
         Observable.concat(memory , network)
+                .first("first")
                 .subscribe(s -> {
                     memoryCache = "memory";
                     System.out.println("--------------subscribe:" + s);
