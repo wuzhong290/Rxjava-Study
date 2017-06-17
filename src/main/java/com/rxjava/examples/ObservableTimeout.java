ce0b2f3bc6ee01faf3c4e379e3fe52e32508bcd5
wuzhong <419479707@qq.com>
2017-06-17 17:27:54 +0800

    demo

--- src/main/java/com/rxjava/examples/ObservableTimeout.java
+++ src/main/java/com/rxjava/examples/ObservableTimeout.java
@@ -4,7 +4,9 @@ import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.BiFunction;
+import io.reactivex.functions.Predicate;
 import io.reactivex.internal.schedulers.ImmediateThinScheduler;
 import io.reactivex.plugins.RxJavaPlugins;
 import io.reactivex.schedulers.Schedulers;
@@ -23,17 +25,19 @@ public class ObservableTimeout {
         Observable<String> memory = Observable.create(new ObservableOnSubscribe<String>(){
                     @Override
                     public void subscribe(@NonNull ObservableEmitter<String> e) throws Exception {
-                        try {
-                            Thread.sleep(200);
-                        }catch (Exception ex){
-                            e.onError(new Exception("sleep"));
-                        }
-                        System.out.println(Thread.currentThread().getName() + "memory");
-                        //e.onError(new Exception("memory-Exception"));
+                        System.out.println(Thread.currentThread().getName() + ":memory");
                         e.onNext("memory");
+                        e.onComplete();
                     }
                 })
                 .timeout(100, TimeUnit.MILLISECONDS)
+                .retry(3, new Predicate<Throwable>() {
+                    @Override
+                    public boolean test(@NonNull Throwable throwable) throws Exception {
+                        System.out.println(Thread.currentThread().getName() +throwable.getMessage()+ ":memory-Predicate");
+                        return true;
+                    }
+                })
                 .onErrorReturn(error ->{
                     System.out.println(Thread.currentThread().getName() + "onErrorReturn");
                     return "onErrorReturn";
@@ -41,18 +45,29 @@ public class ObservableTimeout {
                 .subscribeOn(Schedulers.newThread());
 
         Observable<String> dist = Observable.create(new ObservableOnSubscribe<String>(){
-            @Override
-            public void subscribe(@NonNull ObservableEmitter<String> e) throws Exception {
-                //e.onError(new Exception("dist-Exception"));
-                System.out.println(Thread.currentThread().getName() + "dist");
-                e.onNext("dist");
-                //e.onComplete();
-            }
-        }).subscribeOn(Schedulers.newThread());
+                    @Override
+                    public void subscribe(@NonNull ObservableEmitter<String> e) throws Exception {
+                        //e.onError(new Exception("dist-Exception"));
+                        System.out.println(Thread.currentThread().getName() + "dist");
+                        e.onNext("dist");
+                        e.onComplete();
+                    }
+                })
+                .timeout(100, TimeUnit.MILLISECONDS)
+                .retry(3, new Predicate<Throwable>() {
+                    @Override
+                    public boolean test(@NonNull Throwable throwable) throws Exception {
+                        System.out.println(Thread.currentThread().getName() +throwable.getMessage()+ ":memory-Predicate");
+                        return true;
+                    }
+                })
+                .onErrorReturn(error ->{
+                    System.out.println(Thread.currentThread().getName() + "onErrorReturn");
+                    return "onErrorReturn";
+                })
+                .subscribeOn(Schedulers.newThread());
 
-        Observable<String> network = Observable.just("network").subscribeOn(Schedulers.newThread());
-
-        Observable.zip(memory, dist, new BiFunction<String, String, Object>() {
+        Disposable disposable = Observable.zip(memory, dist, new BiFunction<String, String, Object>() {
                     @Override
                     public Object apply(@NonNull String s, @NonNull String s2) throws Exception {
                         return s+":"+s2;
@@ -69,7 +84,7 @@ public class ObservableTimeout {
 
         System.out.println(Thread.activeCount());
         try {
-            Thread.sleep(1000);
+            Thread.sleep(10000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
