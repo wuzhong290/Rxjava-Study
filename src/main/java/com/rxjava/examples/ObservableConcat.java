d7df546e87c853f6d7a650f85d88998dda0fe404
wuzhong <419479707@qq.com>
2017-06-17 10:47:55 +0800

    demo

--- src/main/java/com/rxjava/examples/ObservableConcat.java
+++ src/main/java/com/rxjava/examples/ObservableConcat.java
@@ -5,6 +5,8 @@ import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.functions.BiFunction;
+import io.reactivex.internal.schedulers.ImmediateThinScheduler;
+import io.reactivex.schedulers.Schedulers;
 import org.apache.commons.lang.StringUtils;
 
 /**
@@ -23,38 +25,56 @@ public class ObservableConcat {
                     e.onComplete();
                 }
             }
-        });
+        }).subscribeOn(ImmediateThinScheduler.INSTANCE);
 
         Observable<String> dist = Observable.create(new ObservableOnSubscribe<String>(){
             @Override
             public void subscribe(@NonNull ObservableEmitter<String> e) throws Exception {
-                System.out.println("dist");
+                System.out.println(Thread.currentThread().getName() + "dist");
                 e.onNext("dist");
                 //e.onComplete();
             }
-        });
+        }).subscribeOn(ImmediateThinScheduler.INSTANCE).cache();
 
-        Observable<String> network = Observable.just("network");
+        Observable<String> network = Observable.just("network").subscribeOn(Schedulers.newThread());
 
         Observable.concat(memory, dist, network)
+                .observeOn(ImmediateThinScheduler.INSTANCE)
                 .subscribe(s -> {
                     memoryCache = "memory";
-                    System.out.println("--------------subscribe:" + s);
+                    System.out.println(Thread.currentThread().getName() + "--------------subscribe1:" + s);
                 });
-        System.out.println("=======================");
-        Observable.merge(memory, network).subscribe(s -> {
-            System.out.println("--------------subscribe:" + s);
-        });
+        System.out.println(Thread.currentThread().getName() + "=======================2");
+        Observable.merge(memory, network)
+                .observeOn(ImmediateThinScheduler.INSTANCE)
+                .subscribe(s -> {
+                    System.out.println(Thread.currentThread().getName() + "--------------subscribe2:" + s);
+                });
 
-        System.out.println("=======================");
+        System.out.println(Thread.currentThread().getName() + "=======================3");
 
         Observable.zip(memory, dist, new BiFunction<String, String, Object>() {
-            @Override
-            public Object apply(@NonNull String s, @NonNull String s2) throws Exception {
-                return s+":"+s2;
-            }
-        }).subscribe(s -> {
-            System.out.println("--------------subscribe:" + s);
-        });
+                    @Override
+                    public Object apply(@NonNull String s, @NonNull String s2) throws Exception {
+                        return s+":"+s2;
+                    }
+                })
+                .observeOn(ImmediateThinScheduler.INSTANCE)
+                .subscribe(s -> {
+                    System.out.println(Thread.currentThread().getName() + "--------------subscribe3:" + s);
+                });
+
+        System.out.println(Thread.currentThread().getName() + "=======================4");
+
+        Observable.zip(network, dist, new BiFunction<String, String, Object>() {
+                    @Override
+                    public Object apply(@NonNull String s, @NonNull String s2) throws Exception {
+                        return s+":"+s2;
+                    }
+                })
+                .observeOn(ImmediateThinScheduler.INSTANCE)
+                .subscribe(s -> {
+                    System.out.println(Thread.currentThread().getName() + "--------------subscribe4:" + s);
+                });
     }
 }
