05b2b2c1ccf768742f7c287e187376255807785f
wuzhong <419479707@qq.com>
2017-06-19 17:33:01 +0800

    demo

--- src/main/java/com/rxjava/examples/HelloWorld.java
+++ src/main/java/com/rxjava/examples/HelloWorld.java
@@ -6,48 +6,31 @@ package com.rxjava.examples;
 
 import io.reactivex.Flowable;
 import io.reactivex.Observable;
-import io.reactivex.Observer;
-import io.reactivex.annotations.NonNull;
-import io.reactivex.disposables.Disposable;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 public class HelloWorld {
     public static void main(String[] args) {
         Flowable.just("Hello world").subscribe(System.out::println);
+
+        System.out.println(new HelloWorld().helloWorld());
     }
+
+
     public String helloWorld(){
-        DemoObserver observer = new DemoObserver();
-        Observable<String> observable = Observable.just("Hello"); // provides datea
-        observable.subscribe(observer); // Callable as subscriber
-        return observer.getResult();
+        Future<String> f = Observable.just("Hello").toFuture();
+        if (f.isDone()) {
+            try {
+               return f.get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+
+            }
+        }
+        return "";
     }
 
-}
-
-class DemoObserver implements Observer<String>{
-    private String result = "";
-    @Override
-    public void onSubscribe(@NonNull Disposable d) {
-        System.out.println(Thread.currentThread().getName() + d.isDisposed() + "--------------onSubscribe");
-    }
-
-    @Override
-    public void onNext(@NonNull String o) {
-        System.out.println(Thread.currentThread().getName() + "--------------onNext:" + o);
-        result = o;
-    }
-
-
-    @Override
-    public void onError(@NonNull Throwable e) {
-        System.out.println(Thread.currentThread().getName() + e.getMessage()+ "--------------onError");
-    }
-
-    @Override
-    public void onComplete() {
-        System.out.println(Thread.currentThread().getName() + "--------------onComplete");
-    }
-
-    public String getResult() {
-        return result;
-    }
 }
