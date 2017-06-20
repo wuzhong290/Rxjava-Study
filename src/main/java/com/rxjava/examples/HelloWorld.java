0e7a47d2c2a0be81a02dec286cd6a80705e13a2b
googlewu <wuzhongyoho@gmail.com>
2017-06-20 10:05:09 +0800

    demo

--- src/main/java/com/rxjava/examples/HelloWorld.java
+++ src/main/java/com/rxjava/examples/HelloWorld.java
@@ -11,6 +11,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 public class HelloWorld {
+
     public static void main(String[] args) {
         Flowable.just("Hello world").subscribe(System.out::println);
 
